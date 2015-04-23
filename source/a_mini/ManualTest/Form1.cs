26b403a1b3f1e8c9b1e3eaad2e0601d1e76679cd
win <wintercoredev@gmail.com>
2015-04-23 14:44:37 +0700

    06: add manual test project

---
+++ source/a_mini/ManualTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+ 
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using CsQuery;
+using CsQuery.HtmlParser;
+using CsQuery.Utility;
+
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+using Description = NUnit.Framework.DescriptionAttribute;
+namespace ManualTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CQ cq = CQ.Create("<button>Boo!</button>");
+            IHTMLButtonElement buttonElement = cq["button"].FirstElement() as IHTMLButtonElement;
+            Assert.IsNotNull(buttonElement);
+        }
+    }
+}

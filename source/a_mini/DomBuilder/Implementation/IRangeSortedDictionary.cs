beb88ab9bd637e46b1dfe47fa351935b596d4271
win <wintercoredev@gmail.com>
2015-05-28 06:33:34 +0700

    24: removed moved files

--- source/a_mini/DomBuilder/Implementation/IRangeSortedDictionary.cs
+++ source/a_mini/DomBuilder/Implementation/IRangeSortedDictionary.cs
@@ -8,7 +8,7 @@ namespace CsQuery.Implementation
     /// <summary>
     /// A sorted dictionary that allows lookup by range.
     /// </summary>
-    interface IRangeSortedDictionary<TKey,TValue> : IDictionary<TKey[], TValue>
+    public interface IRangeSortedDictionary<TKey, TValue> : IDictionary<TKey[], TValue>
     {
         /// <summary>
         /// Return all keys starting with subKey
